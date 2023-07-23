cb0cef3f7838c34860caa484efe4ad85b460858f
kedwani <mahmoudnasser11566@gmail.com>
2023-07-23 13:28:15 -0700

    auto push

--- 0x00-hello_world/test.c
+++ 0x00-hello_world/test.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-int main(void)  {
-  printf("hello\n");
-  
-
-
-  return (0);
-
-
-
+int main(void)
+{
+printf("hello by printf\n");
+puts("hello by puts");
+printf("just to verify auto new line of puts\n");
+putchar("f\n");
+return (0);
 }
