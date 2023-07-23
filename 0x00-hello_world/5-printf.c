49acc7e8689219256c341cb3528ed932e7708b49
kedwani <mahmoudnasser11566@gmail.com>
2023-07-23 15:03:58 -0700

    auto push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+/*
+ *
+ *main - entry point
+ *
+ *description : to print string
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
