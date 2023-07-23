9cd065cc03681426fac8518d59283dbccc97fbee
kedwani <mahmoudnasser11566@gmail.com>
2023-07-23 15:43:50 -0700

    auto push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**
+11;rgb:0000/0000/0000/**
  *main -entry point
  *
  *description : mesh hqool
@@ -10,7 +10,7 @@
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of a int : %d byte(s)\n", sizeof(int));
+	printf("Size of an int : %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
