bac8448ebf17b95b019ca8b43b593de9f5447189
kedwani <mahmoudnasser11566@gmail.com>
2023-07-24 16:51:34 -0700

    auto push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,9 +17,9 @@ int main(void)
 	/* your code goes there */
 	if (n>0)
 		     printf("%d is positive\n", n);
-	else if (n=0)
+	else if (n==0)
 			  printf("%d is zero\n", n);
 		else
-			printf("%D is negative\n". n);
+			printf("%D is negative\n", n);
 	return (0);
 }
