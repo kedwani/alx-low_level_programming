16d17087c4562de6daf3ad0665ed3e1a87c47318
kedwani <mahmoudnasser11566@gmail.com>
2023-07-26 08:42:57 -0700

    auto push

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48 ; num < 58 ; ++num)
+	{
+	putchar(num);
+	}
+	for (num = 65 ; num < 71 ; ++num)
+	{
+	putchar(num);
+	}
+	putchar(10);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main (void)
+{
+	int num = 10;
+	printf("%x",num);
+}
