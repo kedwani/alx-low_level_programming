80a032ee99592443e596d903f894c6a357d461bb
kedwani <mahmoudnasser11566@gmail.com>
2023-07-27 16:02:34 -0700

    auto push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,20 @@
-#include "stdio.h"
 #include "main.h"
-
+#include <stdio.h>
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+void main (void)
 {
 void print_alphabet(void)
 	{
 	char ch = 'a';
 
 	do {
-	putchar(ch);
+	_putchar(ch);
 	++ch;
 	} while (ch <= 'z');
-	putchar (10);
-}}
+	_putchar (10);
+}
+}
