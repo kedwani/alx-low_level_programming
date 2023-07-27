45e10fdfcfe98ce94dd69342f01b11fcd96db6e8
kedwani <mahmoudnasser11566@gmail.com>
2023-07-27 16:25:51 -0700

    auto push

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Return:0
+ */
+
+void print_alphabet_x10(void);
+{
+	int i
+	for (i = 1; i >= 10; ++i)
+		{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+		}
+	_putchar('\n');
+
+}
