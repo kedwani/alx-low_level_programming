3b2a6bb031187b5b2b08e716fe46dd31e55cdbbd
kedwani <mahmoudnasser11566@gmail.com>
2023-07-28 06:58:30 -0700

    auto push

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,45 +6,42 @@
  */
 void print_times_table(int n)
 {
-	int a, b, c;
+	int i, j, k;
 
-	if (n <= 15 && n >= 0)
+	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= n; ++a)
+		for (i = 0; i <= n; i++)
 		{
-			for (b = 0; b <= n; ++b)
+			for (j = 0; j <= n; j++)
 			{
-				c = a * b;
-				if (b == 0)
+				k = j * i;
+				if (j == 0)
 				{
-					_putchar(c + '0');
-				}
-				if (c < 10 && b != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(c + '0');
-				}
-				else if (c >= 10)
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
-				}
-				else if (c >= 100)
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((c / 100) + '0');
-					_putchar(((c % 100) - (c % 10)) + '0');
-					_putchar((c % 10) + '0');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
-                        _putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
