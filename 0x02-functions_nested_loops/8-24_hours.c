8cfbe3458dd74409a91e81a7a405742585430f3d
kedwani <mahmoudnasser11566@gmail.com>
2023-07-28 04:29:03 -0700

    auto push

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+11;rgb:0000/0000/0000
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
@@ -16,6 +16,7 @@ void jack_bauer(void)
 			{
 				_putchar(a + '0');
 				_putchar(a % 10 + '0');
+				_putchar(':');
 				_putchar(b + '0');
 				_putchar(b % 10 + '0');
 				_putchar('\n');
