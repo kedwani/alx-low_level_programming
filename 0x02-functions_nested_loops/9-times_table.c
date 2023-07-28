b208e7dec53f9ee794b080e27049105f2c5ee469
kedwani <mahmoudnasser11566@gmail.com>
2023-07-28 05:03:52 -0700

    auto push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,8 @@ void times_table(void)
 		for (b = 0; b < 10; ++b)
 		{
 			c = a * b;
-			_putchar(c + '0');
+			_putchar((c / 10) + '0');
+			_putchar(c % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
