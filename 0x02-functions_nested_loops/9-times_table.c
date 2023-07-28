93dd8a09e3007e4a9d9e8096aa6a7b05f5e1b5dd
kedwani <mahmoudnasser11566@gmail.com>
2023-07-28 05:22:19 -0700

    auto push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,24 @@ void times_table(void)
 		for (b = 0; b < 10; ++b)
 		{
 			c = a * b;
-			_putchar(c);
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
 
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
 		_putchar('$');
 		_putchar('\n');
