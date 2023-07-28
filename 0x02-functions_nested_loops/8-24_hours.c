6a09ead14c4e291eb2c76b84a8aaa4533f561a23
kedwani <mahmoudnasser11566@gmail.com>
2023-07-27 20:00:43 -0700

    auto push

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,16 +9,16 @@ void jack_bauer(void)
 	int a, b, c, d;
 
 
-	for (a = 0; a <= 9; ++a)
+	for (a = 0; a <= 2; ++a)
 	{
-		for (b = 0; b <= 9; ++b)
+		for (b = 0; b <= 3; ++b)
 		{
-			for (c = 0; c <= 9; ++c)
+			for (c = 0; c <= 6; ++c)
 			{
 
 				for (d = 0; d <= 9; ++d)
 				{
-					_putchar(a+ '0');
+					_putchar(a + '0');
 					_putchar(b + '0');
 					_putchar(':');
 					_putchar(c + '0');
