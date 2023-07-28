aab3248880bff3781f915ad49a8656ef63982cd9
kedwani <mahmoudnasser11566@gmail.com>
2023-07-28 06:23:34 -0700

    auto push

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,16 +7,27 @@
  */
 void print_to_98(int n)
 {
-       while (n >= 98)
-       {
-	       if (n !=98)
-	       printf("%d, ", n);
-	       else
-	       printf("%d", n);
-	       --n;
-       }
-      
-
-
-       printf("\n");
+	if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+			--n;
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+			++n;
+		}
+	}
+	printf("\n");
 }
