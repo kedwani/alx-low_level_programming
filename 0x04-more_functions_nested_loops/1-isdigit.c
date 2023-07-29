1d805ef750a5126a3164d3b37c0a34b9c61fbd7f
kedwani <mahmoudnasser11566@gmail.com>
2023-07-29 12:18:58 -0700

    auto push

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: the character to be checked
- * Return: 1 if c is uppercase, 0 otherwise
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
+
 {
 	if (c <= '0' && c >= '9')
 		return (1);
