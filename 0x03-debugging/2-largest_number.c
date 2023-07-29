59c7790c20104ac7fc7848f31d009efebb465e65
kedwani <mahmoudnasser11566@gmail.com>
2023-07-29 08:19:28 -0700

    auto push

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,16 +12,19 @@ int largest_number(int a, int b, int c)
 
 	if ((a > b && b > c) || (a > c && c > b))
 	{
-		largest = a;
+	largest = a;
 
 	else if ((b > a && a > c) || (b > c && c > a))
 	{
 	largest = b;
 	}
-	else
+	else if ((c > a && a > b) || (c > b && b > a))
 	{
 	largest = c;
 	}
-
+	else
+	{
+	largest = a;
+	}
 	return (largest);
 }
