8575c628c437588ed25f73b732c269e25670c060
kedwani <mahmoudnasser11566@gmail.com>
2023-07-29 16:31:36 -0700

    auto push

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,20 +6,16 @@
  */
 void more_numbers(void)
 {
-	int i, a, b;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (a = 0; a <= 14; a++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar((a % 10) + '0');
-			if (a >= 9 && a != 14)
-				_putchar('1');
-
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
-
-
-
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
