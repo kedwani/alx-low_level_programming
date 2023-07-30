252978092be39c8e0537ff20eb334c140dba238e
kedwani <mahmoudnasser11566@gmail.com>
2023-07-29 17:20:39 -0700

    auto push

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+/**
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+
+		for (a = 1; a <= size; a++)
+		{
+
+			for (b = 1; b <= size; b++)
+			{
+				_putchar('#');
+
+
+			}
+			_putchar('\n');
+
+
+		}
+	}
+	else
+		_putchar('\n');
+}
