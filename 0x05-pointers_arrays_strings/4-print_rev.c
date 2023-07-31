7dabe846267db6551828ab9eedb28cbe8dabec7f
kedwani <mahmoudnasser11566@gmail.com>
2023-07-31 08:16:49 -0700

    auto push

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i;
+		for (i = 0; *s != '\0'; ++s)
+			++i;
+		for (; i != 0; --i)
+			putchar(i);
+
+
+}
