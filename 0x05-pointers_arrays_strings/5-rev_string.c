eba16192ece790854c73f90685d9401e52d87a8a
kedwani <mahmoudnasser11566@gmail.com>
2023-08-04 16:47:05 -0700

    hack

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,20 +4,3 @@
  * @s: string to be reversed
  */
 void rev_string(char *s)
-{
-	int i;
-	char ary[999];
-	int a=0;
-
-		for (i = 0; *s != '\0'; ++s)
-			++i;
-		--s;
-		for (; i != 0; --s)
-		{
-			i--;
-			ary[a] = *s;
-			a++;
-		}
-		ary[a+1] = '\0';
-		*s = ary[0];
-}
