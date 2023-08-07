c63fa239764e03199593a92ec5b8f0e3b5d4a87d
kedwani <mahmoudnasser11566@gmail.com>
2023-08-07 08:53:36 -0700

    auto push

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,3 +4,26 @@
  * @s: string to be reversed
  */
 void rev_string(char *s)
+{
+	int i;
+		for (i = 0; *s != '\0'; ++s)
+			++i;
+	int len = i;
+	char a[i];
+	int c=0 ;
+	do
+	{
+		a[c] = s[i-1];
+
+		--i;
+		++c;
+	} while (i>=0);
+
+	a[c]=0 ;
+
+	for (i=0 ; i>=len;i++)
+	{
+		a[i]=a[c];
+		c++;
+	}
+}
