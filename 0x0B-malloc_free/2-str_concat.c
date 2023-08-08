49232b4858178c7d5b919f0991be5e861ea8da24
kedwani <mahmoudnasser11566@gmail.com>
2023-08-08 16:17:18 -0700

    auto push

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,35 +10,40 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int n1 = 0, n2 = 0, j = 0;
-	char *s;
-	int i1, i2;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-/*lengs of chars*/
-	while (s1[n1])
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+
+	if (s1)
 	{
-		++n1;
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
 
-	while (s2[n2])
+	if (s2)
 	{
-		++n2;
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
-	++n2;
-/*allocat memmory*/
-	s = malloc(sizeof(char) * (n1+n2));
-		if (s == NULL)
-			return (NULL);
-/*intialize memmory*/
-		if (s1)
-			for (i1 = 0; i1 < n1; i1++)
-				s[i1]=s1[i1];
-		if (s2)
-			for (i2 = n1 ;i2 <= (n1+n2); ++i2)
-			{
-				s[i2] = s2[j];
-				j++;
-			}
-		s[i2] = '\0';
-		return (s);
+	s3[i] = '\0';
+
+	return (s3);
 }
