d6b66318e5d096b8d72cf4fe6583de0e967c92ef
kedwani <mahmoudnasser11566@gmail.com>
2023-08-08 13:55:03 -0700

    auto push

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-11;rgb:0000/0000/0000* *create_array - creates an array of chars,
+ *create_array - creates an array of chars,
  * and initializes it with a specific char
  * @size: size of the array to create
  * @c: char to initialize the array c
