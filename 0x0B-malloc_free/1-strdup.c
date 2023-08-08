bd620e1d5044b8b9c5e313a3a4b34006e47196c6
kedwani <mahmoudnasser11566@gmail.com>
2023-08-08 15:15:27 -0700

    auto push

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "main.h"
+11;rgb:0000/0000/0000#include "main.h"
 
 /**
  * *_strdup - copies the string given as parameter
@@ -12,17 +12,16 @@ char *_strdup(char *str)
 	int i, n;
 	char *s;
 /*check for null str*/
-	if (str == 0)
-		return (0);
+	if (str == NULL)
+		return (NULL);
 /*calculate first string length*/
 	for (i = 0; str[i] != '\0'; i++)
 /*allocate memmory for new string*/
-		s = malloc(sizeof(*s)*(i + 1));
-	if (s == 0)
-		return (0);
+		s = malloc(sizeof(char)*(i + 1));
+	if (s == NULL)
+		return (NULL);
 /*intialize memmory*/
-	for (n = 0; n < i; ++n)
+	for (n = 0; str[i] != '\0'; ++n)
 		s[n] = str[n];
-	s[n] = str[n];
 	return (s);
 }
