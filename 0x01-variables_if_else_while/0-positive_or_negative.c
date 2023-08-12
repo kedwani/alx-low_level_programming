bcda47016702984c65c18f85abe20ffe3cb34cc7
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-12 16:44:36 +0300

    auto push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,14 +16,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	if (n>0)
-		printf ("%d is positive\n",n);
-	if (n==0)
-		printf ("%d is zero\n",n);
-	if (n<0)
-		printf ("%d is negative\n",n);
-       
-
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
 
 	return (0);
 }
