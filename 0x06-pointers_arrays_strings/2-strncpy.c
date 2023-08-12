8f3a331c59361fbe23fd7123dd65d9bc0b331813
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-12 22:03:43 +0300

    auto push

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (src[i])
+		i++;
+	while (dest[j])
+		j++;
+	if (i >= n)
+		for (k = 0; k < n; k++)
+			dest[j + k] = src[k];
+	else
+	{
+		for (k = 0; src[k] != '\0'; k++)
+			dest[j + k] = src[k];
+		while (k < n)
+		{
+			dest[k] = '\0';
+			k++;
+		}
+	}
+}
