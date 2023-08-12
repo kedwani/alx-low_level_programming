5079b0088f21454a1c28f2d6abaec3ae7bd40a9a
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-12 18:10:58 +0300

    auto push

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char ch = 'a';
-
-	do {
-	putchar(ch);
-	++ch;
-	} while (ch <= 'z');
-	putchar (10);
+	char ch = a;
+	while (ch <= z)
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
 	return (0);
 }
