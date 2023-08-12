653db58cda4ed0bb47475f482b0302df90e058bc
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-12 23:00:10 +0300

    auto push

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,6 +10,7 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+/*
 	int i = 0;
 	int j = 0;
 	int k = 0;
@@ -31,11 +32,11 @@ char *_strncpy(char *dest, char *src, int n)
 			k++;
 		}
 	}
-/*
+*/
 	for (i = 0; i < n && src[i] != '\0'; i++)
                    dest[i] = src[i];
                for ( ; i < n; i++)
                    dest[i] = '\0';
-*/
+
 	return (dest);
 }
