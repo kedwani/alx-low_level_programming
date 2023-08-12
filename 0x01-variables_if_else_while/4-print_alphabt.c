c878e2ae6d276dd8107b18786c741b6d9cff764f
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-12 19:32:18 +0300

    auto push

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,10 @@ int main(void)
 	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ++ch)
-		{
-	if (ch != 'e' && ch != 'q')
-	putchar(ch);
-		}
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
 	putchar(10);
 	return (0);
 }
