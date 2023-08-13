3319b2b20ba709a2cf06ab34e003d19d0d7ce5e9
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-13 13:45:39 +0300

    auto push

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int loop = 0 ;
+
+	while (s1[loop] = s1[loop] && (s1[loop] != '\0' && s2[loop] != '\0'))
+		++loop;
+	if (s1[loop] = s1[loop])
+		retuen (0);
+	else if (s1[loop] > s2[loop])
+		return (15);
+	else
+		return (-15);
+}
