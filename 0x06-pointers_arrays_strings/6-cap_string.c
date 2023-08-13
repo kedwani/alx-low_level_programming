2ed10a70de4a453b4bc7f580e625463a7df22c95
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-13 16:32:02 +0300

    auto push

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+
+	(i = 0; i != '\0'; ++i)
+	{
+		j = i - 1;
+		if (s[i] >= 97 && s[i] <= 122)
+			if (j = 10 || j = 34 || j = 9 || j = 32 || j = 40 || j = 41 || j = 44 || j = 46 || j = 59 || j = 33 || j = 63 || j = 123 || j = 125)
+				s[i] -= 32;
+	}
