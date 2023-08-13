74c57ec9f157db40b7287bb5b3ba57241005612f
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-13 16:01:52 +0300

    auto push

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,9 +9,7 @@ void reverse_array(int *a, int n)
 	int i;
 	int f;
 
-	if (n % 2 == 0)
-		n++;
-	for (i = 0; i != ((n - 1) / 2); i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		f = a[(n - 1) - i];
 		a[(n - 1) - i] = a[i];
