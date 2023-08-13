3845eae8d50833d9d232dc5405fae07dc7ee91f6
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-13 19:02:37 +0300

    auto push

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,8 +11,10 @@ char *leet(char *s)
 	int i, j;
 	char let[5] = {'A', 'E', 'O', 'T', 'L'};
 	char num[5] = {'4', '3', '0', '7', '1'};
+
 	for (i = 0; s[i] != '\0'; ++i)
 		for (j = 0; j < 5; j++)
 			if (s[i] == let[j] || s[i] == (let[j] + 32))
 				s[i] = num[j];
+	return (s);
 }
