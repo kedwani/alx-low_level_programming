f7625997dd25f82effedd98c67406e081dab7b94
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-15 00:52:40 +0300

    auto push

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,10 +10,5 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	} *d;
+	struct dog *d = {*name, age, *owner};
 }
