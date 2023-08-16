a699f4349235927312bf477396ebdbcc181dae43
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-16 21:42:21 +0300

    auto push

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,7 +16,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; ++i)
 		for (j = 0; accept[j] != '\0'; ++j)
-			if (s[i] == accept[j])
+			if (accept[j] == s[i])
 				k++;
 	return (k);
 
