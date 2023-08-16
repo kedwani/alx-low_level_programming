24d4f62bdd59b23f74f5d7c0541de84877ed986a
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-16 22:28:11 +0300

    auto push

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,12 +15,16 @@ char *_strpbrk(char *s, char *accept)
 	int f = 0;
 
 	for (i = 0; s[i] != '\0'; ++i)
+	{
 		for (j = 0; accept[j] != '\0'; ++j)
 			if (accept[j] == s[i])
 			{
 				f = 1;
 				break;
 			}
+		if (f == 1)
+			break;
+	}
 	if (f == 1)
 		return (&s[i]);
 	else
