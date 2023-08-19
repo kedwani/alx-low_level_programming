cdee1f38716da9b2afaedddeb062fd650c1bfd7c
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-19 21:44:44 +0300

    auto push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,12 +22,14 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(c + '0');
+				_putchar(',');
 			}
 			else
 			{
 				_putchar(' ');
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
+				_putchar(',');
 			}
 		}
 			_putchar('\n');
