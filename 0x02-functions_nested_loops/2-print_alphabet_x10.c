f5c071943613bfc22b7156befa78aa18ad0effd4
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-19 18:25:08 +0300

    auto push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	char ch;
 	int  num;
 
-		for (num = 1; num <=10; num++)
+		for (num = 1; num <= 10; num++)
 		{
-			for (ch = 'a';ch <= 'z'; ch++)
+			for (ch = 'a'; ch <= 'z'; ch++)
 			{
 				_putchar(ch);
 
