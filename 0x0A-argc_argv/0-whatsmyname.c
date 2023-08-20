786123c197076c3eb80b0ac67c6c41e17d71c883
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-20 15:03:14 +0300

    auto push

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-int main(char* argv[])
+int main(int argc ,char* argv[])
 {
-	printf("%s",argv[0]);
+	if (argc > 0)
+		printf("%s\n",argv[0]);
 	return (0);
 }
