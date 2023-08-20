c68ee5c44f2deb15db6073101e33e480f622730d
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-20 15:59:02 +0300

    auto push

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#inculde <stdio.h>
+#include "main.h"
+
+/**
+ * main - print all argu
+ *
+ * @argv : arguments to be printed
+ *
+ * @argc : numbers of argues
+ *
+ * Return: alwayes 0
+ *
+ **/
+
+int main(int argc,char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
