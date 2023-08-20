7d189973803cf60dcad5721212642d59de895b33
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-20 16:07:39 +0300

    auto push

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * main - multiply two arg
+ * @argv : arguments to be multiplyed
+ * @argc : numbers of argues
+ * Return: alwayes 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		printf("%d/n", argv[1] * argv[2]);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
