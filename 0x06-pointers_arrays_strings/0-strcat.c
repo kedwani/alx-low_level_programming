2814d5d46f5bf8823f16f705b2910d6fb3a88f1d
kedwani <mahmoudnasser11566@gmail.com>
2023-08-20 05:09:05 -0700

    hack

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len1 = 0;
+	int len2 = 0;
+	int i = 0;
+	
+/*counting src length */
+	while (src[len1])
+		++len1;
+	while (dest[len2])
+		++len2;
+	for (len2 = len1;len2<())
+
+
+
+}
