28cfcb9243beefb4070e259b6e253f66bf69623b
kedwani <mahmoudnasser11566@gmail.com>
2023-08-20 05:23:16 -0700

    auto push

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -print num of arguments
+ *
+ *Return : alwayes zero
+ *
+ **/
+int main (int argc)
+{
+
+	printf("%d\n", argc);
+
+	return (0);
+}
