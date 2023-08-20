759c216bd5e2ec06aea67e962590b82fe08747f8
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-20 17:09:50 +0300

    auto push

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,10 +13,7 @@ int main(int argc, char *argv[])
 {
 	if (argc > 1)
 	{
-		int a = atoi(argv[1]);
-		int b = atoi(argv[2]);
-
-		printf("%d/n", a * b);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 	else
