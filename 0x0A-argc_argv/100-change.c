9988a82ba6e4564766f2e91ef418cf5713596e40
kedwani linux <mahmoudnasser11566@gmail.com>
2023-08-21 16:36:04 +0300

    auto push

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,10 +12,8 @@
  */
 int main(int argc, char *argv[])
 {
-	/*sum is the change .. rest is the rest of money after every operation */
 	int a = atoi(argv[1]);
-	int sum = 0;
-	int rest = a;
+	int sum = 0, rest = a, i;
 /*check for argc not equal 2*/
 	if (argc != 2)
 	{
@@ -28,23 +26,27 @@ int main(int argc, char *argv[])
 /*the prog*/
 	if (a / 25 > 0)
 	{
-		sum += (a / 25);
-		rest = a - (sum * 25);
+		i = (rest / 25);
+		sum += i;
+		rest = a - (i * 25);
 	}
 	if (rest / 10 > 0)
 	{
-		sum += (rest / 10);
-		rest -= (sum * 10);
+		i = (rest / 10);
+		sum += i;
+		rest -= (i * 10);
 	}
 	if (rest / 5 > 0)
 	{
-		sum += (rest / 5);
-		rest -= (sum * 5);
+		i = (rest / 5);
+		sum += i;
+		rest -= (i * 5);
 	}
 	if (rest / 2 > 0)
 	{
-		sum += (rest / 2);
-		rest -= (sum * 2);
+		i = (rest / 2);
+		sum += i;
+		rest -= (i * 2);
 	}
 	if (rest / 1 > 0)
 		sum += (rest / 1);
