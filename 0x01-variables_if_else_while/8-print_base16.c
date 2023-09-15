f7127c0997b9d5f1ea77118348f3f72f2ef66968
kedwani <mahmoudnasser11566@gmail.com>
2023-09-15 13:17:41 -0700

    auto push

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,9 @@ int main(void)
 	int num;
 
 	for (num = 48 ; num < 58 ; ++num)
-	{
-	putchar(num);
-	}
+		putchar(num);
 	for (num = 97 ; num < 103 ; ++num)
-	{
-	putchar(num);
-	}
+		putchar(num);
 	putchar(10);
 	return (0);
 }
