06b08fb8f647b344102afec91e4d6481ac749dc0
kedwani linux <mahmoudnasser11566@gmail.com>
2023-09-16 18:43:38 +0300

    auto push

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,9 +23,9 @@ int main(void)
 		{
 			for (k = j + 1; k <= 9; ++k)
 			{
-				putchar (i);
-				putchar (j);
-				putchar (k);
+				putchar (i + '0');
+				putchar (j + '0');
+				putchar (k + '0');
 				if (i != 7 || j != 8 || k != 9)
 				{
 					putchar(44);
