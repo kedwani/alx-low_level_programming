3bfbde33a4547c741edf1f4904dbcc5d14ec3df4
kedwani linux <mahmoudnasser11566@gmail.com>
2023-09-16 17:28:08 +0300

    auto push

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i <= 57; ++i)
+	{
+		for (j = i; j <= 57; ++j)
+		{
+			for (k = j; k <= 57; ++k)
+			{
+				if (i != j && j != k && i != k)
+				{
+					putchar (i);
+					putchar (j);
+					putchar (k);
+					if (i != 55 || j != 56 || k != 57)
+					{
+						putchar(44);
+						putchar (32);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
