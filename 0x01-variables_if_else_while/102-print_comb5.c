05fea62f6173314735b7b75ee1b81fe8f30c9f78
kedwani linux <mahmoudnasser11566@gmail.com>
2023-09-18 14:38:40 +0300

    auto push

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ int main(void)
 	for (i = 0; i < 99; ++i)
 		for (j = i + 1; j <= 99; j++)
 		{
-			putchar(i / 10);
-			putchar(i % 10);
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
 			putchar(' ');
-			putchar(j / 10);
-			putchar(j % 10);
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 			if (i != 98)
 			{
 				putchar(',');
