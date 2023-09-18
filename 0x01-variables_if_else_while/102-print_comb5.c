786706cf93908013ee7b494583d32dfc93b5a3d6
kedwani linux <mahmoudnasser11566@gmail.com>
2023-09-18 14:37:40 +0300

    auto push

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
-
 /**
  * main - write 4 combination
  *
- * Return : always zero
+ * Return: always zero(0)
  *
  *
  */
 int main(void)
 {
-	int i, j, k, l;
+	int i, j;
 
-	
-
-
-
-
-
-
+	for (i = 0; i < 99; ++i)
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar(i / 10);
+			putchar(i % 10);
+			putchar(' ');
+			putchar(j / 10);
+			putchar(j % 10);
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	return (0);
 }
