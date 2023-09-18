e8683fb1bbe008f036d9c7c155053524b6938cfd
kedwani linux <mahmoudnasser11566@gmail.com>
2023-09-18 19:09:36 +0300

    auto push

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,8 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-
-=======
->>>>>>> 786123c197076c3eb80b0ac67c6c41e17d71c883
 /**
  * reverse_array - reverses an array of integers
  * @a: array to be reversed
@@ -10,25 +6,12 @@
  */
 void reverse_array(int *a, int n)
 {
-<<<<<<< HEAD
-	int i, j, tmp;
+	int temp;
 
-	j = n - 1;
-
-	for (i = 0; i < n / 2; i++)
+	for (int i = 0; i < n / 2; i++)
 	{
-		tmp = a[i];
-		a[i] = a[j];
-		a[j--] = tmp;
-=======
-	int i;
-	int f;
-
-	for (i = 0; i < n / 2; i++)
-	{
-		f = a[(n - 1) - i];
-		a[(n - 1) - i] = a[i];
-		a[i] = f;
->>>>>>> 786123c197076c3eb80b0ac67c6c41e17d71c883
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
 	}
 }
