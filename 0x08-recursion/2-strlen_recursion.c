0e53fd404f0680efa02bfeabfde73130fb7d4a8b
kedwani linux <mahmoudnasser11566@gmail.com>
2023-09-25 19:40:35 +0300

    auto push

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
-
-
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
 int _strlen_recursion(char *s)
 {
-	int i;
-	if (*s != '\0')
-	{
-		_strlen_recursion(s + 1);
-	}
-	else
-		i = -1;
-	i++;
-	return (i);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
