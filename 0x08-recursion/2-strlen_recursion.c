589a5d7c3f786b7f51247f9e0260910881b8a3cc
kedwani linux <mahmoudnasser11566@gmail.com>
2023-09-25 19:29:50 +0300

    auto push

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,14 +4,15 @@
 
 int _strlen_recursion(char *s)
 {
-	string = alo \0
 	int i;
 	if (*s != '\0')
 	{
 		_strlen_recursion(s + 1);
-		i++;
+
 	}
+
 	else
 		i=0;
-	return (medhat);
+	i++;
+	return (i);
 }
