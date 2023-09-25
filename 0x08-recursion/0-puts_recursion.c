76269e0dccd73baafc5172e7a9497e480c3b24c2
kedwani linux <mahmoudnasser11566@gmail.com>
2023-09-25 18:55:30 +0300

    auto push

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,9 @@
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
-	_putchar(*s);
-	_puts_recursion(s + 1);
-	_putchar('\n');
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+		_putchar('\n');
 }
