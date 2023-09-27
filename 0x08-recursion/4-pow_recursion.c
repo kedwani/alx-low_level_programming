e653de840ae517be9ba8292550ec3637b7998a1a
kedwani linux <mahmoudnasser11566@gmail.com>
2023-09-27 17:30:31 +0300

    auto push

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,11 +11,10 @@ int _pow_recursion(int x, int y)
 {
 	if (y == 0)
 		return (1);
-	else if (y < 0)
-	{
-		--y;
-		return (x * _pow_recursion(x, y));
-	}
-	else
+	if (y < 0)
 		return (-1);
+
+	--y;
+	return (x * _pow_recursion(x, y));
+
 }
