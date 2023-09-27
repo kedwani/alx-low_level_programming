2769d6119d8063273f8b84db48fc6df2ea5b0958
kedwani linux <mahmoudnasser11566@gmail.com>
2023-09-27 19:28:32 +0300

    auto push

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
-int prime (int n ,int i);
+int prime(int n, int i);
 /**
  *is_prime_number - is the main function
  *
- *Return : (0) if num is
+ *Return: alwayes (0) if num is
  *
  *@n : is the num
  */
@@ -18,11 +18,13 @@ int is_prime_number(int n)
 /**
  *prime - is the main function
  *
- *Return : (0) if num is
+ *Return: alwayes (0) if num is
  *
  *@n : is the num
+ *
+ *@i : is parameter
  */
-int prime (int n ,int i)
+int prime(int n, int i)
 {
 	if (n != i && n % i == 0)
 		return (0);
@@ -30,5 +32,5 @@ int prime (int n ,int i)
 		return (1);
 	if (n % i != 0)
 		return (1);
-	return (1 * prime(n,i+1));
+	return (1 * prime(n, i + 1));
 }
