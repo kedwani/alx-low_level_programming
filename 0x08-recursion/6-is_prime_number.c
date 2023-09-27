94bc1a96ea74e8c3ec1552f3b8a023e76c079fb4
kedwani linux <mahmoudnasser11566@gmail.com>
2023-09-27 19:21:01 +0300

    auto push

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+int prime (int n ,int i);
+
+int is_prime_number(int n)
+{
+	if (n < 0)
+		return (0);
+	return (prime(n, 2));
+}
+int prime (int n ,int i)
+{
+	if (n != i && n % i == 0)
+		return (0);
+	if (i > n)
+		return (1);
+	if (n % i != 0)
+		return (1);
+	return (1 * prime(n,i+1));
+}
