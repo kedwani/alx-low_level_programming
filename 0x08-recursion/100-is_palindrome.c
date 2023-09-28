36a419b40e7d4b3f14f9d5eb4d87c32f88d8b9dc
kedwani <mahmoudnasser@sandbox_my_pc>
2023-09-28 10:25:19 -0700

    auto push

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,26 @@
+#include "main.h"
+int _strlen_recursion(char *s);
+int my_func (char *s, int i, int x);
+
+int is_palindrome(char *s)
+{
+  int i = _strlen_recursion(s);
+  return (my_func(s, i - 1, 0));
+}
+
+
+int _strlen_recursion(char *s)
+{
+  if (*s == '\0')
+    return (0);
+  return (1 + _strlen_recursion(s + 1));
+}
+int my_func (char *s, int i, int x)
+{
+  if (x >= i)
+    return (1);
+  if (s[x] == s[i])
+    return (1 * my_func(s, i - 1, x + 1));
+  if (s[x] != s [i])
+    return (0);
+}
