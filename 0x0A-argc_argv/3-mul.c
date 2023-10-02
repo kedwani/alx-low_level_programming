3e483b0ec8bae9437136653f38ce780794814ac3
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-02 22:54:41 +0300

    auto push

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,17 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
-
-	if (argc != 3)
+	if (argc == 3)
 	{
-		printf("Error\n");
-		return (1);
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
+
+		printf("%d\n",a * b);
+		return (0);
 	}
 	else
 	{
-		printf("%d\n",a * b);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
 }
