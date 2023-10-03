afc7c374263bce97d06f4b7bbf6a47b145d93f76
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-03 13:19:44 +0300

    auto push

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,20 +11,21 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s3;
-	unsigned int i, j, len1, len2;
+	unsigned int i, j,;
 
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
+	for (i = 0; s1[i] != "\0"; ++i)
+		;
+	for (j = 0; s2[j] != '\0'; ++j)
+		;
+	--i;
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	s3 = malloc(sizeof(char) * (i + j));
 	if (s3 == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		s3[i] = s1[i];
-	for (j = 0; j < (len1 + len2); ++j)
+	for (j = 0; s2[j] != '\0' ; ++j)
 		s3[i + j] = s2[j];
 	s3[i + j] = '\0';
 	return (s3);
