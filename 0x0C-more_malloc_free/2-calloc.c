f0763080067eebe1540039e0c7ea90d7ed807232
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-08 15:45:49 +0300

    auto push

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,15 +1,15 @@
 #include "main.h"
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
+	char *p;
 	unsigned int i;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
-	if (p == NULL)
+	if (*p == '\0')
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
+		p[i] = '0';
 	return (p);
 }
