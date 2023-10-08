94cde0cb216729b8a3a17917c2c96b9127813429
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-08 12:59:37 +0300

    auto push

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, i2;
+	void *p;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (i2 = 0; s2[i2] != '\0'; i2++)
+		;
+	if (n = i2)
+		p = malloc(i+i2+1);
+	else
+		p = malloc(n+i+1);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] !='\0'; ++i)
+		if (s1[i] != NULL)
+			p[i] = s1[i];
+	for (i2 = 0; i2 < n; i2++)
+		if (s2[i2] != NULL)
+			p[ i + i2] = s2[i2];
+	p[i+i2] = '\0';
+	return (p);
+}
