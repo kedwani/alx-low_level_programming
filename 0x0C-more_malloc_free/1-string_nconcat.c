7784fe0ecfc2f677df2f9aaa6dcb39c1b8e3d1c3
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-08 13:09:23 +0300

    auto push

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, i2;
+	unsigned int i = 0, i2 = 0;
 	char *p;
-
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (i2 = 0; s2[i2] != '\0'; i2++)
-		;
+	if (s1 != NULL)
+		for (i = 0; s1[i] != '\0'; i++)
+			;
+	if (s2 != NULL)
+		for (i2 = 0; s2[i2] != '\0'; i2++)
+			;
 	if (n == i2)
 		p = malloc(i+i2+1);
 	else
 		p = malloc(n+i+1);
 	if (p == NULL)
 		return (NULL);
-
+	if (s1 != NULL)
 	for (i = 0; s1[i] !='\0'; ++i)
-		if (s1[i] != '\0')
-			p[i] = s1[i];
-	for (i2 = 0; i2 < n; i2++)
-		if (s2[i2] != '\0')
+		p[i] = s1[i];
+	if (s2 != NULL)
+		for (i2 = 0; i2 < n; i2++)
 			p[ i + i2] = s2[i2];
 	p[i+i2] = '\0';
 	return (p);
