ed5bc4c259cdc690b1d4ee993d5cdf8175dd4472
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-08 19:48:17 +0300

    auto push

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ *
+ * Return: pointer to the newly allocated memory block
+ */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == old_size)
@@ -16,25 +25,25 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	else if (new_size > old_size)
 	{
-		char *old= (char*)ptr;
+		char *old = (char*)ptr;
 		char *p = malloc(new_size);
 		unsigned int i;
 
 		for (i = 0; i < old_size; i++)
 			p[i] = old[i];
 		free(ptr);
-		return(p);
+		return (p);
 	}
 	else if (new_size < old_size)
 	{
-		char *old= (char*)ptr;
+		char *old = (char*)ptr;
 		char *p = malloc(new_size);
 		unsigned int i;
 
 		for (i = 0; i < new_size; i++)
                         p[i] = old[i];
 		free(ptr);
-		return(p);
+		return (p);
 	}
 	else
 		return (NULL);
