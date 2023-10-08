7257d8aa32708d73a6720c7f8b334be211df534f
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-08 16:03:54 +0300

    auto push

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,12 +4,12 @@ int *array_range(int min, int max)
 {
 	int *p;
 	int i;
+
 	if (min > max)
 		return(NULL);
 	p = malloc((max - min) * sizeof(int));
 	if (p == NULL)
 		return (NULL);
-	i = min;
 	for (i = 0 ; p[i] != max; ++i)
 		p[i] = min + i;
 	p[i] = max;
