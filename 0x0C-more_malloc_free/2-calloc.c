d8a5f988eb0413cfddfdf0a1d585ee9980f002b1
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-08 15:53:16 +0300

    auto push

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,13 @@
 #include "main.h"
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
