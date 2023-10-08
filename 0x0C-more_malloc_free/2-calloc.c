73e2c8293451bfb38e5e35633a98aadea2ddc123
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-08 15:14:06 +0300

    auto push

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,14 @@
+#include "main.h"
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	int i;
+	if (size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		p[i] = '0';
+	return (p);
+}
