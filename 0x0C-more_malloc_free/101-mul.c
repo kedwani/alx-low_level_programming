f2f47e7ac660371233484a95ded6a44e96feeae3
kedwani <mahmoud@my_tablet.com>
2023-10-09 15:53:51 +0200

    auto push

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,21 +1,25 @@
-main(int argc , char **argv)
+#include "main.h"
+
+int main(int argc , char **argv)
 {
+	int i, j;
+  long unsigned int num1 = atoi(argv[1]);
+  long unsigned int num2 = atoi(argv[2]);
+  long unsigned int mul = num1 * num2;
 
   if (argc != 3)
   {
     printf("ERROR");
     return (98);
   }
-  for (i=1; argv[i] != '\0';i++)
+  for (i=1; argv[i] != NULL; i++)
     for (j=0;argv[i][j] != '\0';j++)
       if (argv[i][j] > '9' || argv[i][j] <'0')
 	{
 	  printf("ERROR");
 	  return (98);
 	}
-  long unsigned int num1 = atoi(argv[1]);
-  long unsigned int num2 = atoi(argv[2]);
-  long unsigned int mul = num1 * num2;
-  printf 
 
+  printf ("%lu\n",mul);
+  return (0);
 }
