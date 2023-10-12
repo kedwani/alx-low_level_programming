d74dab8ec0ae45757a23d3593fff0482263658d3
kedwani <my tablet @termux.com>
2023-10-12 16:07:28 +0000

    auto push

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,7 +17,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (size != NULL && array != NULL && action != NULL)
+	if (size != '\0' && action != NULL)
 		for (i = 0; i < size; ++i)
 			action(array[0]);
 }
