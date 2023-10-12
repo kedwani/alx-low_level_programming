2943b7790e46bd46afea3bdd9ada6b5da9ab2969
kedwani <my tablet @termux.com>
2023-10-12 15:52:53 +0000

    auto push

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+
+/**
+ * array_iterator - is a fumction to give all elemnts of array @array as
+ * a parameter of funtion @action
+ *
+ * @array : is the array to pass itis elemnts
+ *
+ * @size : is the size of @array
+ *
+ * @action : is the pointer to function to pass parameter to
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; ++i)
+		action(array[0]);
+}
