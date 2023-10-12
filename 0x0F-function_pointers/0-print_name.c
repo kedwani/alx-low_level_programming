c605a3654df990f8687869e99bae82531350d10e
kedwani <mahmoud@my_tablet.com>
2023-10-12 15:53:34 +0300

    hack

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,16 @@
 #include "function_pointers.h"
 
+
+/**
+ *
+ *print_name - is a fumction to print a name
+ *
+ *@name : is the name to be printed
+ *
+ *@(*f)(char*) is the pointer to function to print the name
+ */
+
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (f != NULL)
