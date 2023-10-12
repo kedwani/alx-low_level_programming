30727a5a83118566112a501a767054b958d68c80
kedwani <my tablet @termux.com>
2023-10-12 15:58:25 +0000

    auto push

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef size_t unsigned int
+typedef unsigned int size_t;
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
