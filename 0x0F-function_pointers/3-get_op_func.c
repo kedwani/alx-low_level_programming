828fff878d6e161c6bd56709795b20d300724ffe
kedwani <mahmoud@myphoneterminal>
2023-10-13 00:21:05 +0300

    hack

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,19 @@
+int (*get_op_func(char *s))(int, int)
+{
+  op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+  int i = 0;
+  while (i < 6)
+    {
+      if (s == ops[i][0])
+	return (ops[i][1]);
+	i++;
+    }
+  return (NULL);
+}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,12 +10,14 @@ int main(int arg,char **argv)
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[3]);
 	char op = argv[2];
+	long int res ;
 
 	if ((op == '/' || op == '%' ) && num2 == 0)
 	{
 		printf("Error\n");
 		return (100);
 	}
-	get_op_func(argv[2])(num1,num2);
-
+	res = int (*get_op_func(argv[2]))(num1, num2);
+	printf("%ld",res);
+	retuen (0);
 }
