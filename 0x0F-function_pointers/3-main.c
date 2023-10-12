4de6e6054b14bf4766ea9a93e87e6cd75c256274
kedwani <my tablet @termux.com>
2023-10-12 22:22:57 +0000

    auto push

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,14 +8,14 @@ int main(int argc, char *argv[])
 		int res ;
 		int num1 = atoi(argv[1]);
 		int num2 = atoi(argv[3]);
-		char s = argv[2][0];
+		char *s = argv[2];
 
-		if ((s == '/' || s == '%' ) && num2 == 0)
+		if ((s[0] == '/' || s[0] == '%' ) && num2 == 0)
 		{
 			printf("Error\n");
 			return (100);
 		}
-		res = (*get_op_func(argv[2]))(num1, num2);
+		res = (*get_op_func(s))(num1, num2);
 		if (res == '\0')
 		{
 			printf("Error\n");
