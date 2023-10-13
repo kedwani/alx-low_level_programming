53c2197585369c686ae84ea8794198178b283d66
kedwani <mahmoudnasser@sandbox_my_pc>
2023-10-13 11:05:48 -0700

    auto push

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,14 +4,14 @@ int sum_them_all(const unsigned int n, ...)
 {
   int res = 0;
   unsigned int i;
+  va_list ptr;
   
   if (n == 0)
     return (0);
-  va_list ptr;
   va_start(ptr,n);
   for (i=0;i<n;++i)
     {
-      res += va_arg(prt;int);
+      res += va_arg(ptr,int);
     }
   va_end(ptr);
   return (res);
