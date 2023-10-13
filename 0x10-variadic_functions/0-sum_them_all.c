8756e40fe4cf67673108360572e8a04262da436c
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-13 15:00:46 +0300

    auto push

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,14 +5,17 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
+	unsigned int i;
+
 	if (n == 0)
 		return (0);
 	else
 	{
 		va_list ptr = n;
 		va_start(ptr,n);
+		va arg;
 		for (i = 0; i < n; ++i)
-			sum +=v_arg;
+			sum += va_arg;
 		va_end;
 		return(sum);
 	}
