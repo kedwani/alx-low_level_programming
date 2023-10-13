dd13433be4c98f5e2c9469b9960d5f241e265455
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-13 18:11:00 +0300

    auto push

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,10 +17,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; ++i)
 		{
 			s = va_arg(ptr, char*);
-			if (s != NULL)
-				printf("%s", s);
-			else
+			if (s == NULL)
 				printf("nil");
+			else
+				printf("%s", s);
 			if (separator != NULL && i != (n - 1))
 				printf("%s", separator);
 		}
