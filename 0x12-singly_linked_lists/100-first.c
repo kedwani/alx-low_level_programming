d7a37e9aacb14588273d0d608697be4fd4bdb6e3
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-22 20:54:52 +0300

    auto push

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main
+ * first_print - prints a sentence before the main
  * function is executed
  */
-void first(void)
+void first_print(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
