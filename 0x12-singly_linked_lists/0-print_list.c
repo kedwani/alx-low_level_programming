895e2030c77e90354a6d5c7bfecfbaa2e9148cda
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-22 10:05:49 +0300

    auto push

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 size_t print_list(const list_t *h)
 {
 	list_t  *now;
-	long unsigned int i;
+	unsigned long int i;
 
 	now = h;
 	for (i = 0; now != NULL; ++i)
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 		if (now->str == NULL)
 			printf("[0] (nil)");
 		else
-			printf("[%u] %s\n", new->len , next->str);
+			printf("[%u] %s\n", new->len, next->str);
 		now = now->next;
 	}
 	return (i);
