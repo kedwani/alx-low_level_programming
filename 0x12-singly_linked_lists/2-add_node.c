1c9d317f881a26898b2be2b1a9bd5ca5c54bdc60
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-22 10:35:56 +0300

    auto push

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,19 +12,12 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int len = 0;
-
-	while (str[len])
-		len++;
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
-
+	new->next = *head;
 	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
-
-	return (*head);
+	head == new;
+	return (new);
 }
