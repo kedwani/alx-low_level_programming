3c8592159bbfab126d69a1699bcea2ab2ee0bed6
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-22 20:02:02 +0300

    auto push

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,14 +8,13 @@
 void free_list(list_t *head)
 {
 	list_t *ptr, *temp;
-	ptr = head->next;
 
-	while (temp)
+	ptr = head
+		while (ptr)
 	{
 		temp = ptr->next;
 		free(ptr);
 		ptr = temp;
 
 	}
-	free(head);
 }
