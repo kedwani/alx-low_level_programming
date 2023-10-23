04f61119e6866f116c755520cacb1510c04ce8fd
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-23 22:59:28 +0300

    auto push

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,18 +12,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *h = *head, *new_node;
-
+/*check for null list */
 	if (!h)
 		return (NULL);
-	for (i = 0; i < idx - 1; ++i)
+	/* go for the desired position */
+	for (i = 0; i < idx; ++i)
 	{
 		if (!h)
 			return (NULL);
 		h = h->next;
 	}
+	/*create the new node */
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
+/*intialize the new node & append it*/
 	new_node->n = n;
 	new_node->next = h->next;
 	h->next = new_node;
