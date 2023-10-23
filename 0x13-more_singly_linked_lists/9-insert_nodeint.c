32de747d1ee9d53ef905269b0d1de329689ccc6d
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-23 23:33:57 +0300

    auto push

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		h = h->next;
 	if (h == NULL)
 		return (NULL);
-		new_node->next = h->next;
+	new_node->next = h->next;
 	h->next = new_node;
 	return (new_node);
 }
