802442d81d60789e031e9f1c315449dc3e5b6be3
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-23 17:22:30 +0300

    auto push

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,8 +9,9 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *new_node, *temp;
 /* create the new node*/
-	listint_t *new_node = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
 	if (!new_node)
 		return (NULL);
@@ -18,8 +19,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 /* append the new node */
-	while (head->next)
-		head = head->next;
-	head->next = new_node;
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new_node;
 	return (new_node);
 }
