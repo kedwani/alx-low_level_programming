0800b6d2264c8bccd2f6b0ca5d43e20c6c1591ba
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-24 20:24:53 +0300

    auto push

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,21 +1,36 @@
 #include "lists.h"
-
 /**
- *print_listint_safe - is a function to print content of linked list.
- *@head : is the header node.
- *Return: the number of printed nodes.
-*/
-
+ * print_listint_safe - function that prints a linked list with a loop safely.
+ * @head: pointer to the 1st node of the linked list
+ * Return: new_node
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i = 0;
-	if (!head)
-		exit(98);
-	while (head)
+	const listint_t *tmp_n = NULL;
+	const listint_t *l_n = NULL;
+	size_t counter = 0;
+	size_t new_n;
+
+	tmp_n = head;
+	while (tmp_n)
 	{
-		printf("[%p] %d\n",(void*)head, head->n);
-		head = head->next;
-		++i;
+		printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
+		counter++;
+		tmp_n = tmp_n->next;
+		l_n = head;
+		new_n = 0;
+		while (new_n < counter)
+		{
+			if (tmp_n == l_n)
+			{
+				printf("-> [%p] %d\n", (void *)tmp_n, tmp_n->n);
+				return (counter);
+			}
+			l_n = l_n->next;
+			new_n++;
+		}
+		if (!head)
+			exit(98);
 	}
-	return (i);
+	return (counter);
 }
