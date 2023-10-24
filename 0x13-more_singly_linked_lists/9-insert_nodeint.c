53ed1f4d9d54a6440b8f128142d99d0c2b94de61
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-24 13:01:37 +0300

    auto push

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,33 +12,37 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *new_node;
+	listint_t *new;
+	if (!*head)
+		return (NULL);
 
-	if (!idx || !*head || !n)
-		return (NULL);
-	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return(NULL);
+	new->n = n;
+	new->next = NULL;
+
 	if (idx == 0)
 	{
-		new_node->n = n;
-		new_node->next = *head;
-		*head = new_node;
+		new->next = *head;
+		*head = new;
 		return (*head);
 	}
-
-	for (i = 0; i < idx - 1; ++i)
+	for (i = 0; i <idx - 1; ++i)
 	{
 		if (!*head)
 		{
-			free(new_node);
+			free(new);
 			return (NULL);
 		}
 		*head = (*head)->next;
 	}
-	new_node->next = (*head)->next;
-	(*head)->next = new_node;
-	return (new_node);
+	if (!*head)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next =(*head)->next;
+	(*head)->next = new;
+	return (new);
 }
