a664a9f350779cfc58e2a1b5966ab512d22a87b3
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-24 19:41:55 +0300

    auto push

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,9 +1,14 @@
 #include "lists.h"
-/***/
+/**
+*reverse_listint - is a function to reverse a list.
+*@head : is the head of list
+*Return: the head of reversed list
+*/
 
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *tmp, *prv;
+
 	if (!head || !*head)
 		return (NULL);
 
