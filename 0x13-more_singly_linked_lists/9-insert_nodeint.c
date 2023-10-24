175809301b8326798e1bd0fe46b6b8fc95a7a428
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-24 13:26:19 +0300

    auto push

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *new, *h = *head;
 
-	if (!*head || !head)
+	if (!*head || !head || !idx || !n)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
