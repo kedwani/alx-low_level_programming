6e9a15752916ba07e8db0ea64ec92a701874a3f1
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-26 18:56:58 +0300

    auto push

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,47 @@
-#include main.h
+#include "main.h"
+unsigned int getsquar(int a, int b);
+
+/**
+ *print_binary - is a function to convert a unit to bin.
+ *@n : s the uint num.
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i;
+	int flag = 0;
+
+	for (i = 31; i >= 0; --i)
+	{
+		if (n >= getsquar(2, i))
+		{
+			_putchar('1');
+			n -= getsquar(2, i);
+			flag = 1;
+			continue;
+		}
+		else if (flag == 1)
+		{
+			_putchar('0');
+			continue;
+		}
+		if (n == 0)
+			break;
+	}
+	_putchar(10);
+	exit(0);
+}
+
+/**
+ *getsquar- is a function to getsquar.
+ *@b : s the power.
+ *@a : is the num
+ *Return: the result.
+ */
+
+unsigned int getsquar(int a, int b)
+{
+	if (b == 0)
+		return (1);
+	return (a * getsquar(a, b - 1));
+}
