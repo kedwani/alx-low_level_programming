beb914768e59c3c91c2c7234b69ceac1587d93ab
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-29 01:18:33 +0200

    auto push

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,20 +9,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int sum = 0;
-	unsigned int i, len = 0;
+	int i, len = 0;
 
 	if (!b)
 		return (0);
 	while (b[len])
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
 		len++;
 	}
 
 	for (i = len - 1; i >= 0; ++i)
 	{
-		else if (b[i] == '1' && i == (len - 1))
+		if (b[i] == '1' && i == (len - 1))
 			sum += 1;
 		else if (b[i] == '1')
 			sum += 2 << (len - 2 - i);
