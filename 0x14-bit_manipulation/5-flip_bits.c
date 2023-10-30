4a88a67c0c83a4db30c2b9d83105d8f081d6ee3b
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-30 12:29:57 +0200

    auto push

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,17 +10,13 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int diff;
-	int counter;
+	int counter, i;
 
 	diff = n ^ m;
 	counter = 0;
-
-	while (diff)
-	{
-		counter++;
-		diff &= (diff - 1);
-	}
-
+	i = sizeof(m) * 8;
+	for (--i; i >= 0; --i)
+		if (((diff >> i) & 1) == 1)
+			++counter;
 	return (counter);
 }
-
