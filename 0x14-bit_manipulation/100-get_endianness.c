7ab5a64312a5853df78b221f7da4bc0930838245
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-30 12:57:08 +0200

    auto push

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,8 +1,12 @@
 #include "main.h"
+/**
+ *get_endianness- if a function to check endianness.
+ *Return: check.
+ */
 int get_endianness(void)
 {
 	int i = 1;
-	char *ptr = (char*) &i;
+	char *ptr = (char *) &i;
 
 	if (*ptr == '1')
 		return (0);
