9d8b031198985f3efa86502b7fd03af1b9ab5d12
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-01 15:06:19 +0200

    auto push

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int o, r, w;
 
 	if (filename == NULL)
-                return (0);
+		return (0);
 	buf = malloc(sizeof(size_t) * letters);
 	if (buf == NULL)
 		return (0);
