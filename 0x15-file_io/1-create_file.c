5716a4e449e2f3c0330e083606834b108afedf3f
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-01 15:22:07 +0200

    auto push

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int o, w;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
+	o = open(filename, O_RDWR | O_CREAT | O_EXCL | O_TRUNC, 0600);
+	if (o == -1)
+		return (-1);
+	if (text_content)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+
+		w = write(o, text_content, strlen(text_content));
+		if (w == -1)
+			return (-1);
 	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
-		return (-1);
-
 	close(o);
-
 	return (1);
 }
