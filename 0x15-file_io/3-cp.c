4267f6439001170bced304a6abf253f1fb703373
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-01 17:35:39 +0200

    auto push

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,7 @@ int main(int argc, char ** argv)
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
@@ -45,7 +45,7 @@ int main(int argc, char ** argv)
 
 	if (from == -1 || r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (to == -1 || w == -1)
