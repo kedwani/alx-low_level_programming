883d66257614c631a36738638c66aec718dd30b1
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-01 14:58:19 +0200

    auto push

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -31,7 +31,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 		free(buf);
 	}
-	w = write(STDOUT_FILENO, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
 	if (w == -1)
 	{
 		return (0);
---
+++ 0x15-file_io/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+int main()
+{
+	int a = read_textfile("textfile_0", 100);
+	printf("/n chars is :%d/n",a);
+	return (0);
+}
