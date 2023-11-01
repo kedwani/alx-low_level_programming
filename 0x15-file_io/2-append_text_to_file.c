2008ec2878eb1930808a49d82ccfe5111132d9f2
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-01 16:16:09 +0200

    auto push

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,24 +11,16 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int o, w;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	w = write(o, text_content, strlen(text_content));
+	if (w == -1 || o == -1)
 		return (-1);
-
 	close(o);
-
 	return (1);
+
 }
