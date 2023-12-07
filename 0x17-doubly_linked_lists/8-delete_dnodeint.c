d4e713a80c2978159ad784e8a17313319dab9bee
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-07 17:01:05 +0200

    auto push

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,1 +1,19 @@
 #include "lists.h"
+/**
+ * sum_dlistint - Computes the sum of stored in a doubly linked list.
+ * @head: A pointer to the head of the doubly linked list.
+ *
+ * Return: The sum of all the integers in the doubly linked list.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int i = 0;
+	dlistint_t *h;
+
+	while (h)
+	{
+		i = h->n;
+		h = h->next;
+	}
+	return (i);
+}
