141421688045bd749423ac1bda8c24ab29b8ba86
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-07 19:24:18 +0200

    auto push

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,8 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (hd)
 		{
-			new->prev = hd;
-			new->next = hd->next;
+			new->next = hd;
 		}
 		*h = new;
 		return (new);
