07be97897b71f45d35f32f82f60954eada310604
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-07 20:25:47 +0200

    auto push

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -30,8 +30,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		if (i == index)
 		{
-			h->prev->next = h->next;
-			h->next->prev = h->prev;
+			h->prev->next = NULL;
+			if (h->next)
+				h->next->prev = h->prev;
 			free(h);
 			return (1);
 		}
