2d153c51b5ff5cd584351dd962efa94270c21dc4
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-07 14:04:59 +0200

    auto push

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,15 @@
-#include <lists.h>
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	if (!h)
+		return(0);
+	for (;h != NULL;++i)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+	}
+	return(i);
+}
