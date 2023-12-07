a1bd15054414f115922863fc3981623145a5e2cb
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-07 16:19:08 +0200

    auto push

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+/**
+ * free_dlistint - Frees a doubly linked list.
+ * @head: A pointer to the head of the doubly linked list.
+ *
+ * Description: Frees all nodes in a doubly linked list and the head itself.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	tmp = head;
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+	free(head);
+	free(tmp);
+}
