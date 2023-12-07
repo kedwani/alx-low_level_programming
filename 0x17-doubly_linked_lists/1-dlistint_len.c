71d272854c7f73bbddcb60c3ffdc67a2f560cce2
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-07 14:27:42 +0200

    auto push

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,18 @@
 #include "lists.h"
+/**
+ * dlistint_len - Counts the number of nodes in a doubly linked list.
+ * @h: A pointer to the head of the doubly linked list.
+ *
+ * Return: The number of nodes in the doubly linked list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	while(h)
+	{
+		h = h->next;
+		++i;
+	}
+	return (i);
+}
