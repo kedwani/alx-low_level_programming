723f325344aeed5813c996f2a83594a152be34fa
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-13 21:44:57 +0200

    auto push

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,16 +3,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node;
-	
-	if (hash_table_t == NULL)
+	if (ht == NULL)
 		return (0);
-	index = key_index(key, hash_table_t->size);
-	if (hash_table_t->array[index] == NULL)
+	index = key_index(key, ht->size);
+	if (ht->array[index] == NULL)
 	{
 		new_node = malloc(sizeof(hash_node_t));
-		if (new_node == nuLL)
+		if (new_node == NULL)
 			return (0);
-		new_node->key 
+		new_node->key;
 	}
 
 
