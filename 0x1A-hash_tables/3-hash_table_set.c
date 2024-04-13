fa6f034a09f5e037bb45bf51221c1c9859973a11
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-13 22:33:46 +0200

    auto push

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,10 +12,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
+	{
+		free(new_node);
 		return (0);
+	}
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
+	{
+		free(new_node);
 		return (0);
+	}
 	if (ht->array[index] == NULL)
 		new_node->next = NULL;
 	else
