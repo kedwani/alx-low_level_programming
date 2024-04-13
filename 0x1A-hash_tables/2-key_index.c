673801a49802c335c6e7fd6e8bef5dd9e7aea7de
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-13 21:42:20 +0200

    auto push

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - Generates an index for a key in a hash table.
+ * @key: The key to hash.
+ * @size: The number of slots in the hash table.
+ * Return: index at which key/value pair should be stored in the hash table.
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key)%size);
+	return (hash_djb2(key) % size);
 }
