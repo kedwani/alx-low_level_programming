40a79745ec1323bd5783e34f367d8034ed1f26aa
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-13 19:14:27 +0200

    auto push

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,1 +1,6 @@
-#include <lists.h>
+#include "hash_tables.h"
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key)%size);
+}
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,1 +1,20 @@
-#include <lists.h>
+#include "hash_tables.h"
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+	
+	if (hash_table_t == NULL)
+		return (0);
+	index = key_index(key, hash_table_t->size);
+	if (hash_table_t->array[index] == NULL)
+	{
+		new_node = malloc(sizeof(hash_node_t));
+		if (new_node == nuLL)
+			return (0);
+		new_node->key 
+	}
+
+
+
+}
