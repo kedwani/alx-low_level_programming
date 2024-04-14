a07e61ee601a2e47fb15ae163dbeeb3898e3336e
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-14 17:58:23 +0200

    auto push

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,4 +1,9 @@
 #include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: Pointer to the hash table.
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *tmp;
@@ -6,22 +11,28 @@ void hash_table_print(const hash_table_t *ht)
 	int flag = 0;
 
 	if (ht == NULL)
-		exit (0);
+		return;
+
 	printf("{");
-	for (i = 0; i <= ht->size; ++i)
+
+	for (i = 0; i < ht->size; ++i)
 	{
 		if (ht->array[i] == NULL)
 			continue;
+
 		tmp = ht->array[i];
+
 		while (tmp)
 		{
 			if (flag == 0)
 				printf("'%s': '%s'", tmp->key, tmp->value);
-			if (flag == 1)
+			else
 				printf(", '%s': '%s'", tmp->key, tmp->value);
+
 			flag = 1;
 			tmp = tmp->next;
 		}
 	}
+
 	printf("}\n");
 }
