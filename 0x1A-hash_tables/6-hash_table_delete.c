d87045b8701aa03050f41285c5fce2868061e424
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-14 18:06:44 +0200

    auto push

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,1 +1,30 @@
-#include <lists.h>
+#include "hash_tables.h"
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp, tmp2;
+    unsigned long int i;
+
+    if (ht == NULL)
+        return;
+
+    for (i = 0; i < ht->size; ++i)
+    {
+        if (ht->array[i] == NULL)
+            continue;
+	else
+	{
+		tmp = ht->array[i]->next;
+		free(ht->array[i]->key);
+		free(ht->array[i]->value);
+		free(ht->array[i]);
+		while (tmp)
+		{
+			free(tmp->key);
+			free(tmp->value);
+			tmp2 = tmp->next;
+			free(tmp);
+			tmp = tmp2;
+		}
+	}
+    }
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -50,7 +50,7 @@ int node_create(hash_table_t *ht, const char *key,
 		const char *value, unsigned long int index);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
-
+void hash_table_delete(hash_table_t *ht);
 
 
 
