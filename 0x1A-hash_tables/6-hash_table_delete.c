78b6f4f10c42c6333c6148b9124f61b52db70ada
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-14 18:43:46 +0200

    auto push

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,42 +6,41 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-        hash_node_t *tmp, *tmp2; /* Temporary pointers for node traversal */
-        unsigned long int i; /* Index for iterating through the hash table */
+	hash_node_t *tmp, *tmp2; /* Temporary pointers for node traversal */
+	unsigned long int i; /* Index for iterating through the hash table */
+	/* Return immediately if the hash table pointer is NULL */
+	if (ht == NULL)
+		return;
 
-        /* Return immediately if the hash table pointer is NULL */
-        if (ht == NULL)
-                return;
+	/* Iterate over each slot of the array in the hash table */
+	for (i = 0; i < ht->size; ++i)
+	{
+		/* If the slot is empty, just continue to the next */
+		if (ht->array[i] == NULL)
+		{
+			continue;
+		}
+		else
+		{
+			/* Point to the first node in linked list at array[i] */
+			tmp = ht->array[i]->next;
+			/* Free key and value of head node, then node itself */
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
 
-        /* Iterate over each slot of the array in the hash table */
-        for (i = 0; i < ht->size; ++i)
-        {
-                /* If the slot is empty, just continue to the next */
-                if (ht->array[i] == NULL)
-                {
-                        continue;
-                }
-                else
-                {
-                        /* Point to the first node in the linked list at array[i] */
-                        tmp = ht->array[i]->next;
-                        /* Free the key and value of the head node, then the node itself */
-                        free(ht->array[i]->key);
-                        free(ht->array[i]->value);
-                        free(ht->array[i]);
-
-                        /* Traverse the rest of the linked list */
-                        while (tmp)
-                        {
-                                tmp2 = tmp->next; /* Save next node */
-                                free(tmp->key); /* Free current node's key */
-                                free(tmp->value); /* Free current node's value */
-                                free(tmp); /* Free current node */
-                                tmp = tmp2; /* Move to next node */
-                        }
-                }
-        }
-        /* Finally, free the array and the hash table struct */
-        free(ht->array);
-        free(ht);
+/* Traverse the rest of the linked list */
+			while (tmp)
+			{
+				tmp2 = tmp->next; /* Save next node */
+				free(tmp->key); /* Free current node's key */
+				free(tmp->value); /* Free curent node's value */
+				free(tmp); /* Free current node */
+				tmp = tmp2; /* Move to next node */
+			}
+		}
+	}
+	/* Finally, free the array and the hash table struct */
+	free(ht->array);
+	free(ht);
 }
