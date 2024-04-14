d08245960e93f77d20af0ddf7fbd052a93bbf434
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-14 18:08:26 +0200

    auto push

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,29 +2,29 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *tmp, *tmp2;
-    unsigned long int i;
+	unsigned long int i;
 
-    if (ht == NULL)
-        return;
-
-    for (i = 0; i < ht->size; ++i)
-    {
-        if (ht->array[i] == NULL)
-            continue;
-	else
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; ++i)
 	{
-		tmp = ht->array[i]->next;
-		free(ht->array[i]->key);
-		free(ht->array[i]->value);
-		free(ht->array[i]);
-		while (tmp)
+		if (ht->array[i] == NULL)
+			continue;
+		else
 		{
-			free(tmp->key);
-			free(tmp->value);
-			tmp2 = tmp->next;
-			free(tmp);
-			tmp = tmp2;
+			tmp = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			while (tmp)
+			{
+				free(tmp->key);
+				free(tmp->value);
+				tmp2 = tmp->next;
+				free(tmp);
+				tmp = tmp2;
+			}
 		}
 	}
-    }
+	free(ht);
 }
